27a5eb347ef1e14241cc276b6e44b934ab8a67a7
6002298 <6002298@PC29ATLZ.insags.com>
2022-09-26 12:32:25 +0200

    Ejercicio 4

---
+++ Ejercicio4/src/ejercicio4/repetidos.java
@@ -0,0 +1,49 @@
+package ejercicio4;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class repetidos {
+
+	/* Constantes */
+
+	private static final String ESTA_ES_LA_LISTA_EN_ORDEN_ASCENDENTE = "Esta es la lista en orden ascendente : ";
+	private static final String ESTA_ES_LA_LISTA = "Esta es la lista : ";
+	private static final int _0 = 0;
+	private static final int _7 = 7;
+	private static final int _3 = 3;
+	private static final int _1 = 1;
+
+	/* Proceso */
+
+	public static void main(String[] args) {
+
+		/* Creamos una lista */
+
+		List<Integer> numeros;
+		numeros = new ArrayList<Integer>();
+
+		/* Añadimos valores */
+
+		numeros.add(_1);
+		numeros.add(_3);
+		numeros.add(_7);
+		numeros.add(_7);
+		numeros.add(_0);
+
+		/* Representación de la lista normal y en orden ascendente. */
+
+		for (int i = 0; i < 1; i++) {
+			System.out.println(ESTA_ES_LA_LISTA + numeros);
+			ordenar(numeros);
+			System.out.println(ESTA_ES_LA_LISTA_EN_ORDEN_ASCENDENTE + numeros);
+		}
+	}
+
+	/* Función de ordenar ascendentemente con el método collections.sort() */
+
+	public static void ordenar(List<Integer> numeros) {
+		Collections.sort(numeros);
+	}
+}
---
+++ Pruebas/src/pruebas/ejercicio2Mr.java
@@ -0,0 +1,53 @@
+package pruebas;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ejercicio2Mr {
+
+	/* Constantes */
+
+	private static final String LOS_NUMEROS_SON = "Los numeros son:";
+
+	public static void main(String[] args) {
+
+		/* Creamos la lista */
+
+		List<Integer> numeros;
+		numeros = new ArrayList<Integer>();
+
+		/* Bucle para formar los números aletorios (5) aleatorios con Math.Random */
+
+		for (int i = 0; i < 5; i++) {
+			int numero = (int) (Math.random() * 50 + 1);
+			if (numeros.contains(numero)) {
+				i--;
+			} else {
+				numeros.add(numero);
+			}
+		}
+
+		/* Imprimos por pantalla los distintos resultados */
+
+		System.out.println(LOS_NUMEROS_SON);
+		for (int i = 0; i < 1; i++) {
+			System.out.println(numeros);
+			ordenar(numeros);
+			System.out.println(numeros);
+		}
+
+		/*
+		 * ordenar(numeros);
+		 * 
+		 * for (int i = 0; i < 1; i++) { System.out.println(numeros); }
+		 */
+	}
+
+	/* Funciones */
+	
+	public static void ordenar(List<Integer> numeros) {
+		Collections.sort(numeros);
+	}
+}
+
